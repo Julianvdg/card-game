3112822167df96fe79c1c12d0e04ddbb19bca045
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-09-07 16:50:06 +0200

    Adjust store

--- client/reducers/currentCard.js
+++ client/reducers/currentCard.js
@@ -4,7 +4,11 @@ import { DRAW_CARD } from '../actions/draw-card'
 export default function updateCurrentCard(state = {}, { type, payload }) {
   switch (type) {
     case DRAW_CARD :
-      return payload
+      const newCard = payload.cards[0]
+      return Object.assign(
+        newCard,
+        { remaining: payload.remaining }
+      )
 
     default :
       return state
